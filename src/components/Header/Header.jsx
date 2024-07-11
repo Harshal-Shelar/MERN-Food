129b81906922903b8e13affbd8b9870367251bb8
Harshal-creator <harshalshelar8251@gmail.com>
2024-07-11 21:49:01 +0530

    Second Commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-content">
+                <h2>Order Your favourite food here</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam earum architecto corporis at aliquid nisi dignissimos similique libero aperiam suscipit?</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
